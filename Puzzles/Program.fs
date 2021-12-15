520b569692bd6903d096e697ccf11447e4c16214
Tobias Watzek <spitzfaust@gmail.com>
2021-12-15 07:58:44 +0100

    First half of day 14

--- Puzzles/Program.fs
+++ Puzzles/Program.fs
@@ -1,4 +1,5 @@
-﻿printfn "Day 1:"
+﻿(*
+printfn "Day 1:"
 printfn "- Part One Solution = %d" Day1.solvePartOne
 printfn "- Part Two Solution = %d" Day1.solvePartTwo
 printfn "Day 2:"
@@ -7,3 +8,7 @@ printfn "- Part Two Solution = %d" Day2.solvePartTwo
 printfn "Day 3:"
 printfn "- Part One Solution = %d" Day3.solvePartOne
 //printfn "- Part Two Solution = %d" Day2.solvePartTwo
+*)
+printfn "Day 14:"
+//printfn "- Part One Solution = %d" Day14.solvePartOne
+printfn "- Part Two Solution = %d" Day14.solvePartTwo
